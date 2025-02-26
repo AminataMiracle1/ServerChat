b03d4be5847cd3c4fe742629267c370a2125b23d
amina <2128896@cegepoutaouais.qc.ca>
2025-02-25 22:31:26 -0500

    Great JOB : Ajout de l'historique avec JSON

--- src/main/java/Server.java
+++ src/main/java/Server.java
@@ -1,25 +1,23 @@
 import com.google.gson.Gson;
-
 import java.io.*;
 import java.net.ServerSocket;
 import java.net.Socket;
-import java.sql.SQLOutput;
 import java.util.ArrayList;
 
 public class Server {
 
     private static final String FICHIER = "message.json";
-    private static ArrayList<Message> listeMessages = new ArrayList<>();
+    private static ArrayList listeMessages = new ArrayList<>();
+    // Appeler la fonction
 
     public static void main(String[] args) {
-
+        lireFichier(); // Charger les messages depuis le fichier
         try (ServerSocket serverSocket = new ServerSocket(8888)) {
 
             while (true) {
                 Socket Clientsocket = serverSocket.accept();
                 //// Créer un utilisateur à chaque fois qu'une personne se Connecte
                 Message message = new Message();
-
                 // Cette information pour envoyer au client
                 PrintWriter socketOut = new PrintWriter(Clientsocket.getOutputStream(), true);
                 // C'est ce que nous recevons du client
@@ -28,27 +26,27 @@ public class Server {
                 String ligne;
 
                 socketOut.println("Bienvenue sur chat");
-
+                socketOut.println("Nombre de messages reçus : " + staticMessage());
                 socketOut.println("Entrer votre user name");
                 String username = socketIn.readLine();
                 message.setUserName(username);
-                socketOut.println("Bonjour : " + message.getUnMessage());
+                socketOut.println("Bonjour : " + message.getUserName());
+
                 socketOut.println("Écriver votre message");
-                //
                 while ((ligne = socketIn.readLine()) != null && !ligne.isEmpty()) {
-                    // Sauvegarder le message
+                    // ecrire le message
                     message.ecrireUnMessage(ligne);
-                    listeMessages.add(message);
-                    if (ligne.equals("static")) {
-                        socketOut.println("La taille des messages : " + staticMessage());  // Afficher le nombre de messages
+                    if (ligne.equals("LOG")) {
+                        socketOut.println("*********l'historique des messages*********");
+                        socketOut.println(listeMessages.toString());
                     }
                 }
                 System.out.println("_____Message____ ");
                 System.out.println("Utilisateur : " + message.getUserName());
                 System.out.println(message.getUnMessage());
-                socketOut.println(listeMessages.toString());
+                listeMessages.add(message);
                 System.out.println("____fin________");
-                // Fermer la connexion avec le client
+                // Fermer la connexion avec le client et l'enregistrer avant que le server se ferme
                 sauvegarderMessage();
                 Clientsocket.close();
             }
@@ -66,27 +64,28 @@ public class Server {
             Gson gson = new Gson();
             String json = gson.toJson(listeMessages, ArrayList.class);
             FileWriter fileWriter = new FileWriter(FICHIER);
-            fileWriter.write(gson.toJson(json));
+            fileWriter.write(json);
             fileWriter.close();
         } catch (IOException e) {
             throw new RuntimeException(e);
         }
     }
-
     // Désérialisation
     public static void lireFichier() {
+        // Vérifier que le fichier json exist
         try {
             Gson gson = new Gson();
+            // Vérifier d'abord que le fichier n'est pas  vide
             BufferedReader br = new BufferedReader(new FileReader(FICHIER));
-            etudiantsRecuperes = gson.fromJson(br, Etudiant[].class);
+            listeMessages = gson.fromJson(br, ArrayList.class);
         } catch (IOException e) {
             throw new RuntimeException(e);
         }
     }
 
     // C'est le log des messages
-    public static ArrayList<Message> getListeMessages() {
-        return listeMessages;
+    public static int staticMessage() {
+        return listeMessages.size();
     }
 
 
